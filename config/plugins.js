41b8e7a13186769bd19fd0e70f5dc46465230a88
Robert Peterman <robpeterman573@gmail.com>
2022-04-23 22:47:48 -0400

    Adding some more config to troubleshoot cloudinary/strapi issues

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,14 @@
-if (process.env.NODE_ENV === "production") {
-    module.exports = ({ env }) => ({
-      upload: {
-        provider: "cloudinary",
-        providerOptions: {
-          cloud_name: env("CLOUDINARY_NAME"),
-          api_key: env("CLOUDINARY_KEY"),
-          api_secret: env("CLOUDINARY_SECRET"),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-      // ...
-    });
-  } else {
-    module.exports = {};
-  }
-  
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  },
+});
